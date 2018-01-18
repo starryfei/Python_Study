0efc7e047bfa6e63680b8e33015419420dad0e9c
starryfei <760646630fei@sina.com>
2018-01-18 23:13:27 +0800

    use jieba count number of words

--- MovieSpider.py
+++ MovieSpider.py
@@ -4,6 +4,10 @@ from urllib import request
 from bs4 import BeautifulSoup
 import re
 
+import jieba    #分词包
+import pandas as pd
+
+
 def get_movie_list():
     URL = "https://movie.douban.com/cinema/nowplaying/beijing/"
 
@@ -25,17 +29,17 @@ def get_movie_list():
     return listItem
 
 
-def get_movie_commnts(movie_list,start):
-    comments_url = 'https://movie.douban.com/subject/' + movie_list[0]['id'] + '/comments?start='+str(start) + '&limit=20'
+def get_movie_commnts(movie_list, start):
+    comments_url = 'https://movie.douban.com/subject/' + movie_list[0]['id'] + '/comments?start=' + str(
+        start) + '&limit=20'
     resp_comments = request.urlopen(comments_url)
     comments_data = resp_comments.read().decode("utf-8")
     comments_soup = BeautifulSoup(comments_data, "html.parser")
     comments = comments_soup.find_all("div", id="comments")
     commentsList = comments[0].find_all('div', class_='comment-item')
-    count = comments_soup.find('li',class_='is-active')
-    count_str= count.find('span').get_text()
+    count = comments_soup.find('li', class_='is-active')
+    count_str = count.find('span').get_text()
     num = re.findall(r'[^()]+', count_str)[1]
-    # print(num)
     commentsItem = []
 
     for comments_item in commentsList:
@@ -45,15 +49,32 @@ def get_movie_commnts(movie_list,start):
         commentsItem.append(comments_all)
     return commentsItem
 
+
 if __name__ == "__main__":
     movie_list = get_movie_list()
     content = []
     for i in range(0, 10):
         start = i * 20
-        content.append(get_movie_commnts(movie_list,start))
+        content.append(get_movie_commnts(movie_list, start))
     fh = open("comments.txt", 'w', encoding='utf8')
     str_value = ''
+    cleaned_comments =''
     for s in range(len(content)):
         str_value = str_value + (str(content[s])).strip()
-        fh.write(str_value)
+        pattern = re.compile(r'[\u4e00-\u9fa5]+')
+        filterdata = re.findall(pattern, str_value)
+        cleaned_comments =cleaned_comments+ ''.join(filterdata)
+        fh.write(cleaned_comments)
+    # print(cleaned_comments)
+    # 进行文字统计
+    s = jieba._lcut(cleaned_comments)
+    word_count = pd.DataFrame({'s':s})
+    stop_words = pd.read_csv("stopwords.txt",index_col=False,quoting=3, sep="\t", names=['stopword'],encoding='utf-8')
+    word_count = word_count[~word_count.s.isin(stop_words.stopword)]
+    word_count = word_count.groupby(['s']).size()
+    print(word_count.sort_values(ascending=False))
+
+    # 用词云进行显示
+
+    # word_count.head()
     fh.close()
