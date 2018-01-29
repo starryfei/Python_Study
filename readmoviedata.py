fcd7958bf7fadc4fc9266ac65881da0c71402d6d
yafei.qin <760646630fei@sina.com>
2018-01-29 17:57:56 +0800

    Update readmoviedata.py

--- readmoviedata.py
+++ readmoviedata.py
@@ -13,7 +13,7 @@ for line in open("file\michao.txt",'r',encoding= 'UTF-8'):
 
 segment = jieba._lcut(cleaned_comments)
 word_count = pd.DataFrame({'segment':segment})
-stop_words = pd.read_csv("stopwords.txt",index_col=False,quoting=3, sep="\t", names=['stopword'],encoding='utf-8')
+stop_words = pd.read_csv("file\stopwords.txt",index_col=False,quoting=3, sep="\t", names=['stopword'],encoding='utf-8')
 word_count = word_count[~word_count.segment.isin(stop_words.stopword)]
 # 统计词频
 words_stat = word_count.groupby(by=['segment'])['segment'].agg({"size"})
@@ -22,7 +22,7 @@ word_count = word_count.groupby(['segment']).size()
 # print(word_count.sort_values(ascending=False))
 
 # 用词云进行显示
-wordcloud = WordCloud(font_path="simhei.ttf", background_color="white", max_font_size=80)  # 指定字体类型、字体大小和字体颜色
+wordcloud = WordCloud(font_path="file\simhei.ttf", background_color="white", max_font_size=80)  # 指定字体类型、字体大小和字体颜色
 word_frequence = {x[0]: x[1] for x in words_stat.head(1000).values}
 
 word_frequence_list = []
