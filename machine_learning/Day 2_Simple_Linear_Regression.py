13dd8f4a68337a43f13b43308ee5c53d84c4ceb4
starryfei <760646630fei@sina.com>
2018-09-10 19:03:51 +0800

    Create Day 2_Simple_Linear_Regression.py

---
+++ machine_learning/Day 2_Simple_Linear_Regression.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 设置
+x = np.array([1., 2., 3., 4., 5.])
+y = np.array([1., 3., 2., 3., 5.])
+# 绘图
+plt.scatter(x, y)
+plt.axis([0, 6, 0, 6])
+plt.show()
+# mean平均值,接下来按照公式求解即可
+x_mean = np.mean(x)
+y_mean = np.mean(y)
+num = 0.0
+d = 0.0
+for x_i, y_i in zip(x, y):
+    num += (x_i - x_mean) * (y_i - y_mean)
+    d += (x_i - x_mean) ** 2
+# 所解值
+a = num / d
+b = y_mean - a * x_mean
+y_hat = a * x + b
+# 绘图
+plt.scatter(x, y)
+plt.plot(x, y_hat, color='r')
+plt.axis([0, 6, 0, 6])
+plt.show()
